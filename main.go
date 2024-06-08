e28f1c3f49b26823429dfa90bcff7acf7d2d0c59
PCPedroso <pclpedroso@gmail.com>
2024-06-08 13:53:11 -0300

    Concluido conteudos das aulas

--- main.go
+++ main.go
@@ -27,11 +27,27 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-	produto := NewProduct("Placa de Vídeo", 1250.00)
+
+	produto := NewProduct("Monitor", 1050.00)
 	err = inserirProduto(db, produto)
+
 	if err != nil {
 		panic(err)
 	}
+
+	//err = updateProduto(db, produto)
+	//produto, err = selectProduto(db, "fb4c678e-4191-440c-8d76-192f9a26a0a4")
+	//produtos, err := selectProdutos(db)
+
+	// for _, produto := range produtos {
+	// 	fmt.Printf("Produto: %v possui o valor de %.2f\n", produto.Nome, produto.Preco)
+	// }
+
+	err = deleteProduto(db, "fb4c678e-4191-440c-8d76-192f9a26a0a4")
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func inserirProduto(db *sql.DB, produto *Produto) error {
@@ -43,6 +59,77 @@ func inserirProduto(db *sql.DB, produto *Produto) error {
 	defer stmt.Close()
 
 	_, err = stmt.Exec(produto.ID, produto.Nome, produto.Preco)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func updateProduto(db *sql.DB, produto *Produto) error {
+	// Utilizando Prepare para evitar SQL Injection
+	stmt, err := db.Prepare("update produto set preco = ? where nome = ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(produto.Preco, produto.Nome)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func selectProduto(db *sql.DB, id string) (*Produto, error) {
+	// Utilizando Prepare para evitar SQL Injection
+	stmt, err := db.Prepare("select id, nome, preco from produto where id = ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	var produto Produto
+
+	err = stmt.QueryRow(id).Scan(&produto.ID, &produto.Nome, &produto.Preco)
+	if err != nil {
+		panic(err)
+	}
+
+	return &produto, nil
+}
+
+func selectProdutos(db *sql.DB) ([]Produto, error) {
+	rows, err := db.Query("select id, nome, preco from produto")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var listaProdutos []Produto
+
+	for rows.Next() {
+		var produto Produto
+
+		err = rows.Scan(&produto.ID, &produto.Nome, &produto.Preco)
+		if err != nil {
+			panic(err)
+		}
+
+		listaProdutos = append(listaProdutos, produto)
+	}
+
+	return listaProdutos, err
+}
+
+func deleteProduto(db *sql.DB, id string) error {
+	stmt, err := db.Prepare("delete from produto where id = ?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id)
+
 	if err != nil {
 		panic(err)
 	}
